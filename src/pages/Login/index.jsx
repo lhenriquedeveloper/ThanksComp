e69d3b891725efb6a01bcb2b0a1a4897b0c21cb6
lhenriquedeveloper <sousarodriguesluishenrique@gmail.com>
2022-08-01 19:50:25 -0300

    LoginPage

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,14 +1,18 @@
 import '../../styles/login.sass'
+import Logo from '../../assets/imgs/ThanksCompLogo.png'
 
 export default function Login() {
     return (
         <div className='login-container'>
             <div className='login-box'>
+                <img src={Logo} alt="Logo ThanksComp" />
                 <h2>Faça seu logon:</h2>
                 <input type="email" name="email" id="email" placeholder='Digite seu email:' />
                 <input type="password" name="pass" id="pass" placeholder='Digite sua senha:' />
-                <button id='reg'>Cadastre-se</button>
-                <button id='log'>Logar</button>
+                <div className='btn-box'>
+                    <div className='register-btn'><button>Cadastre-se</button></div>
+                    <div className='login-btn'><button>Logar</button></div>
+                </div>
             </div>
         </div>
     )
