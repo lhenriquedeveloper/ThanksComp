83739fd7472720378976759c9c858d3fce9aa0ca
lhenriquedeveloper <sousarodriguesluishenrique@gmail.com>
2022-08-02 23:04:16 -0300

    Finished LoginScreen

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -8,17 +8,16 @@ export default function Login() {
             <div className='login-box'>
                 <img src={Logo} alt="Logo ThanksComp" />
                 <h2>Faça seu logon:</h2>
-                <input type="email" name="email" id="email" placeholder='Digite seu email:' />
+                <input type="email" name="email" id="email" placeholder='Digite seu e-mail:' />
                 <input type="password" name="pass" id="pass" placeholder='Digite sua senha:' />
                 <div className='btn-box'>
                     <div className='register-btn'><button>Cadastre-se</button></div>
                     <div className='login-btn'><button>Logar</button></div>
                 </div>
-                <div className='google-btn'>
-                    <button id='google-id'>
-                        <div className='google-icon'><FcGoogle size={35} /></div>
-                    </button>
-                </div>
+                <button id='google-id'>
+                    <FcGoogle size={30} className='google-icon' />
+                    Continuar com o Google
+                </button>
             </div>
         </div>
     )
