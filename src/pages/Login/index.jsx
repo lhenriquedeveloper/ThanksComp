9bd8608a85d191b73d259618162f2b305aaa6cda
lhenrique <sousarodriguesluishenrique@gmail.com>
2022-08-03 15:17:21 -0300

    Register + Responsive Login Screen

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,11 @@
 import '../../styles/login.sass'
 import Logo from '../../assets/imgs/ThanksCompLogo.png'
 import { FcGoogle } from 'react-icons/fc'
+import { useNavigate } from 'react-router-dom'
 
 export default function Login() {
+    let navigate = useNavigate();
+
     return (
         <div className='login-container'>
             <div className='login-box'>
@@ -11,7 +14,7 @@ export default function Login() {
                 <input type="email" name="email" id="email" placeholder='Digite seu e-mail:' />
                 <input type="password" name="pass" id="pass" placeholder='Digite sua senha:' />
                 <div className='btn-box'>
-                    <div className='register-btn'><button>Cadastre-se</button></div>
+                    <div className='register-btn'><button onClick={() => { navigate("/register") }}>Cadastre-se</button></div>
                     <div className='login-btn'><button>Logar</button></div>
                 </div>
                 <button id='google-id'>
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,7 +1,29 @@
+import '../../styles/register.sass'
+import { Link } from "react-router-dom";
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+
 export default function Register() {
     return (
         <div>
-            <h1>Register - In Progress</h1>
+            <div className='register-container'>
+                <div className='register-box'>
+
+                    <div className="info-space">
+                        <h2>Cadastro:</h2>
+                        <p>Faça seu cadastro, entre na plataforma e ajude alguém doando algum componente ou adquira para descarte ou para uso próprio.</p>
+                        <Link to="/"><AiOutlineArrowLeft size={20} /> Voltar para o logon</Link>
+                    </div>
+
+                    <div className='form-space'>
+                        <input type="text" name="fullname" id="fullname" placeholder='Nome Completo:' />
+                        <input type="email" name="email" id="email" placeholder='E-mail:' />
+                        <input type="text" name="number" id="number" placeholder='WhatsApp:' />
+                        <input className='city-input' type="text" name="city" id="city" placeholder='Cidade:' />
+                        <input className='uf-input' type="text" name="uf" id="uf" placeholder='UF:' />
+                        <button>Cadastrar</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
